2c48703d6adcabb196134ae49c185158b9aa75e0
Brian Maddy <brian@brianmaddy.com>
2014-07-10 15:08:47 -0500

    got tests running with solr

--- spec/import/ead/importer_spec.rb
+++ spec/import/ead/importer_spec.rb
@@ -9,14 +9,14 @@ describe Ead::Importer do
     it "imports the correct number of items" do
       Blacklight.solr.delete_by_query('*:*', params: {commit: true})
       Ead::Importer.import([ead_file])
-      expect(Blacklight.solr.select('q' => "*:*")['response']['numFound']).to eq 669
+      expect(Blacklight.solr.select(params: {'q' => "*:*"})['response']['numFound']).to eq 14
     end
 
     it "imports items correctly" do
       Ead::Importer.import([ead_file])
-      doc = Blacklight.solr.select('q' => "FIXME")['response']['docs'].first
-      expect(doc['id']).to eq "U DSG/1/1"
-      expect(doc['title_tesim']).to eq ["File. Correspondence received from other poets, filed A-C"]
+      doc = Blacklight.solr.select(params: {'q' => 'id:"U DDH/1"'})['response']['docs'].first
+      expect(doc['id']).to eq "U DDH/1"
+      expect(doc['title_tesim']).to eq ["Photocopy of file '1932 - 38'"]
     end
   end
 
