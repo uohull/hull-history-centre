532fd2e22cce5c17c9e5da64d94a105bc7a01051
val99erie <val99erie@gmail.com>
2014-07-22 17:45:44 -0500

    Refactored code into a helper method

--- app/helpers/show_helper.rb
+++ app/helpers/show_helper.rb
@@ -9,4 +9,26 @@ module ShowHelper
     end
   end
 
+  def breadcrumbs(document)
+    keys = [['collection_title_ss', 'collection_id_ss'],
+            ['series_title_ss', 'series_id_ss']
+    ]
+
+    # If we have the ID for a solr document, the breadcrumb
+    # should be a link to that document.  Else, just print the
+    # breadcrumb's title as a String.
+    crumbs = keys.map do |(title_key, id_key)|
+      breadcrumb_title = document[title_key]
+      if document[id_key]
+        link_to breadcrumb_title, catalog_path(document[id_key])
+      else
+        breadcrumb_title
+      end
+    end
+
+    current_crumb = document['title_tesim'].first
+    crumbs = crumbs + [current_crumb]
+    crumbs = crumbs.compact.join(' &gt; ').html_safe
+  end
+
 end
