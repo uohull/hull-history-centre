f7bfdb46df35fc28055aad9221a69875505e692c
val99erie <valerie@gentian.local>
2014-07-24 16:35:58 -0500

    More work on SIRSI parser

--- lib/import/sirsi/parser.rb
+++ lib/import/sirsi/parser.rb
@@ -4,6 +4,17 @@ module Sirsi
   module Parser
     class << self
 
+      def parse(file)
+        sirsi_data = File.read(file)
+        xml = Nokogiri::XML(sirsi_data)
+        nodes = xml.xpath("//#{Sirsi::LibraryRecord.root_xpath}")
+        records = nodes.map do |record|
+          attrs_for_record(record, Sirsi::LibraryRecord)
+        end
+
+        { library_records: records }
+      end
+
       # Get all the attributes for a single record
       def attrs_for_record(node, record_class)
         record_class.fields_map.inject({}) do |attrs, (field, xpath)|
--- spec/import/sirsi/parser_spec.rb
+++ spec/import/sirsi/parser_spec.rb
@@ -4,8 +4,9 @@ require_relative '../../../lib/import/sirsi'
 describe Sirsi::Parser do
   let(:fixtures_path) { File.expand_path(File.join('spec', 'fixtures', 'sample_sirsi_files')) }
   let(:single_item) { File.join(fixtures_path, 'single_sirsi_record.xml') }
+  let(:sirsi_records) { File.join(fixtures_path, 'full_record_sample.xml') }
 
-  describe 'attrs_for_record' do
+  describe '.attrs_for_record' do
     let(:attrs) {
       xml = Nokogiri::XML(File.read(single_item)).xpath("//#{Sirsi::LibraryRecord.root_xpath}")
       Sirsi::Parser.attrs_for_record(xml, Sirsi::LibraryRecord)
@@ -14,6 +15,13 @@ describe Sirsi::Parser do
     it 'finds the attributes for that record' do
       expect(attrs[:id]).to eq '1667917'
       expect(attrs[:title]).to match(/Abrâegâe des preuves/)
+    end
+  end
+
+  describe '.parse' do
+    it 'returns a hash with attributes for all the library records' do
+      records = Sirsi::Parser.parse(sirsi_records)
+      expect(records[:library_records].count).to eq 3
     end
   end
 end
