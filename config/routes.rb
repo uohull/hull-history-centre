f7c2292f4d890eb020cc18a30d106b19a5065aef
Mark Bussey <mark@curationexperts.com>
2014-08-14 11:55:44 -0500

    Rename /catalog --> /resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,19 @@
 Rails.application.routes.draw do
+  
   root :to => "catalog#index"
-  blacklight_for :catalog
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  
+  # Original route config for 'catalog' spelling
+  # blacklight_for :catalog
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # New route config based on hyhull example
+  # We want to override the the Blacklight catalog route to direct to 'resources' 
+  Blacklight.add_routes(self, except: [:catalog, :solr_document])
+  # We don't add the Blacklight catalog/solr_document routes so that...
+  # ... we can override url route with 'resources'..
+  match 'resources/opensearch', to: 'catalog#opensearch',  as: 'opensearch_catalog', via: :get
+  match 'resources/facet/:id', to: 'catalog#facet', as: 'catalog_facet', via: :get
+  match 'resources', to: 'catalog#index', as: 'catalog_index', via: :get
+  resources :solr_document,  path: 'resources', controller: 'catalog', only: [:show, :update] 
+  resources :catalog, path: 'resources', controller: 'catalog', only:  [:show, :update]
+  # End of catalog/solr_document overides
 end
