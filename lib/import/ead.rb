696b5812c52c6bc286e06f6f3d8c27bd635f50c7
val99erie <val99erie@gmail.com>
2014-08-26 12:01:55 -0500

    Require common files.  Story #40

--- lib/import/ead.rb
+++ lib/import/ead.rb
@@ -1,3 +1,9 @@
+# Load the concerns first
+concerns_dir = File.join(File.dirname(__FILE__), 'concerns')
+Dir[File.join(concerns_dir, '**', '*.rb')].each do |file|
+  require file
+end
+
 Dir[File.join(File.dirname(__FILE__), '**', '*.rb')].each do |file|
   require file
 end
