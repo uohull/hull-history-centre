7a664204d4b51973d274d69d8743e6f0d6db11cd
richgj700 <rich.garbutt@gmail.com>
2014-10-08 17:46:52 +0100

    HHCOC-22: Added layout markup/style for resource type icons

--- app/helpers/hull_history_centre_helper.rb
+++ app/helpers/hull_history_centre_helper.rb
@@ -18,4 +18,47 @@ module HullHistoryCentreHelper
     end
   end
 
+  # Returns the Resource type/Genre from the Solr document
+  def resource_type_from_document(document)
+    resource_type = ""
+    resource_type = document["genre_ssm"] unless document["genre_ssm"].nil?
+    resource_type = resource_type.is_a?(Array) ? resource_type.first : resource_type
+  end
+
+  def display_resource_thumbnail(document)
+    #/assets/dvd-thumb-lg.png
+    #content_tag(:img, nil, url: thumbnail_src_from_document(document) )
+    # format ssi
+    #content_tag(:img, nil, class: "img-responsive" , src: thumbnail_src_from_document(document) )
+    content_tag(:img, nil, class: "img-responsive" , src: "/assets/dvd-thumb-lg.png" )
+  end
+
+  def thumbnail_src_from_document(document)
+    resource_src = resource_type_from_document(document)
+
+    thumbnail_src = case resource_type
+    when "Book"
+      "/assets/dvd-thumb-lg.png"
+    when "Meeting papers or minutes"
+      "calendar-thumb"
+    when "Dataset"
+      "dataset-thumb"
+    when "Presentation"
+      "presentation-thumb"
+    when "Policy or procedure", "Regulation"
+      "policy-thumb"
+    when "Photograph", "Artwork"
+      "image-thumb"
+    when "Thesis or dissertation"
+      "thesis-thumb"
+    when "Handbook"
+      "handbook-thumb"
+    when "Book"
+      "domesday-thumb"
+    else
+      "generic-thumb"
+    end
+
+  end
+
 end
