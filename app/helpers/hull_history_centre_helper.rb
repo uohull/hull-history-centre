3badd3900763667b4d09640382e11015dbc0e171
richgj700 <rich.garbutt@gmail.com>
2014-10-09 16:59:13 +0100

    HHCOC-22: Implemented icons for search results and all underlying resource pages

--- app/helpers/hull_history_centre_helper.rb
+++ app/helpers/hull_history_centre_helper.rb
@@ -18,45 +18,40 @@ module HullHistoryCentreHelper
     end
   end
 
-  # Returns the Resource type/Genre from the Solr document
+  # Returns the Resource type/Format from the Solr document
   def resource_type_from_document(document)
     resource_type = ""
-    resource_type = document["genre_ssm"] unless document["genre_ssm"].nil?
+    resource_type = document["format_ssi"] unless document["format_ssi"].nil?
     resource_type = resource_type.is_a?(Array) ? resource_type.first : resource_type
   end
 
   def display_resource_thumbnail(document)
-    #/assets/dvd-thumb-lg.png
-    #content_tag(:img, nil, url: thumbnail_src_from_document(document) )
-    # format ssi
-    #content_tag(:img, nil, class: "img-responsive" , src: thumbnail_src_from_document(document) )
-    content_tag(:img, nil, class: "img-responsive" , src: "/assets/dvd-thumb-lg.png" )
+    content_tag(:img, nil, class: "img-responsive thumbnail-image" , src: thumbnail_src_from_document(document) )
   end
 
   def thumbnail_src_from_document(document)
-    resource_src = resource_type_from_document(document)
+    resource_type = resource_type_from_document(document)
 
     thumbnail_src = case resource_type
     when "Book"
-      "/assets/dvd-thumb-lg.png"
-    when "Meeting papers or minutes"
-      "calendar-thumb"
-    when "Dataset"
-      "dataset-thumb"
-    when "Presentation"
-      "presentation-thumb"
-    when "Policy or procedure", "Regulation"
-      "policy-thumb"
-    when "Photograph", "Artwork"
-      "image-thumb"
-    when "Thesis or dissertation"
-      "thesis-thumb"
-    when "Handbook"
-      "handbook-thumb"
-    when "Book"
-      "domesday-thumb"
+      asset_path "icon-flat-book.png"
+    when "CD/DVD"
+      asset_path "icon-flat-dvd.png"
+    when "Map"
+      asset_path "icon-flat-map.png"
+    when "Microfilm"
+      asset_path "icon-flat-microfilm.png"
+    # no icons created yet
+    #when "Newspaper"
+      #asset_path "icon-flat-file"
+    #when "Pamphlet"
+      #asset_path "icon-flat-file"
+    #when "Cassette"
+      #asset_path "icon-flat-file"
+    when "Video"
+      asset_path "icon-flat-video.png"
     else
-      "generic-thumb"
+       asset_path "icon-flat-file.png"
     end
 
   end
